9344106dc8c788aa14fe55612a62ed79ba8c8b0f
jhon <fr.jhonk@gmail.com>
2023-06-10 11:08:08 -0500

    setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='double robust did',
+    version='0.1.0',
+    description='Descripción de la librería',
+    url='https://github.com/d2cml-ai/drdid',
+    author='Jhon Flores',
+    author_email='fr.jhonk@gmail.com',
+    packages=['drdid'],
+)
