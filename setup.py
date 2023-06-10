fe4d50f31e5270ee36f618e152fb9f802a5cf1f0
jhon <fr.jhonk@gmail.com>
2023-06-10 13:33:00 -0500

    reg_panel

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='double robust did',
-    version='0.1.4',
+    version='0.1.14',
     description='Descripción de la librería',
     url='https://github.com/d2cml-ai/drdid',
     author='Jhon Flores',
