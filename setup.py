335bc2c9332196229c067ef0b5eb84a6e6238248
jhon <fr.jhonk@gmail.com>
2023-06-16 20:28:21 -0500

    version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from drdid._version_ import version
 setup(
     name='drdid',
     version=version,
-    description='Descripción de la librería',
+    description='Double Robust Difference in Difference y python',
     url='https://github.com/d2cml-ai/drdid',
     author='Jhon Flores',
     license="MIT",
@@ -15,5 +15,10 @@ setup(
         'pandas', 
         'numpy<=1.24.3',
         'statsmodels'
-    ]
+    ],
+    long_description='''
+    Implementation of drdid for Python, R-like syntax with return destructuring using optimized Python code.
+
+    See the original [R package](https://github.com/pedrohcgs/DRDID/tree/master)
+    '''
 )
